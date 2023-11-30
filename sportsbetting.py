9a9447bc4cc9e55e71f925305efa5596f551ab08
Sachin Sharma <32823703+sachinsharma30@users.noreply.github.com>
2023-11-30 12:47:04 -0800

    Add files via upload
    script + ex. csv

---
+++ sportsbetting.py
@@ -0,0 +1,39 @@
+import requests
+import csv
+
+apiKey = "" #put ur own api key
+idURL = "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?regions=us&oddsFormat=american&apiKey=" + apiKey
+
+idResponse = requests.get(idURL)
+data = idResponse.json()
+
+try:
+    gameIDs = [i["id"] for i in data]
+except TypeError:
+    print("invalid API key")
+    exit(1)
+
+props = ["player_pass_yds", "player_reception_yds", "player_rush_yds"]
+
+with open('suggestedPicks.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(['Player', 'o/u', 'Line', 'Stat', 'Odds'])
+
+    for ids in gameIDs:
+        for stat in props:
+            gameURL = f"https://api.the-odds-api.com/v4/sports/americanfootball_nfl/events/{ids}/odds?apiKey={apiKey}&regions=us&markets={stat}&oddsFormat=american"
+            gameResponse = requests.get(gameURL)
+            gameData = gameResponse.json()
+
+            outcomes = [outcome for bookmaker in gameData.get("bookmakers", []) for outcome in bookmaker.get("markets", [])[0].get("outcomes", [])]
+
+            for outcome in outcomes:
+                if int(outcome.get("price", 0)) <= -130:
+                    csvwriter.writerow([
+                        outcome.get('description', ''),
+                        outcome.get('name', ''),
+                        outcome.get('point', ''),
+                        stat,
+                        outcome.get('price', '')
+                    ])
+                    
